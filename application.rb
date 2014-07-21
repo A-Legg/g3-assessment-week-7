5208c192c25594a17bb6952de6a5c7d3cfa66767
Adam Legg <adam.legg@gmail.com>
2014-07-21 11:47:22 -0600

    failed assessment

--- application.rb
+++ application.rb
@@ -14,6 +14,14 @@ class Application < Sinatra::Application
     erb :index
   end
 
+  post '/' do
+    @database_connection.sql("INSERT INTO home (name, message) VALUES ('#{params[:name]}', '#{params[:message]}')")
+    redirect "/"
+
+  end
+
+
+
   get '/continents' do
     all_continents = CountryList.new.continents
     erb :continents, locals: { continents: all_continents }
@@ -22,6 +30,19 @@ class Application < Sinatra::Application
   get '/continents/:continent_name' do
     list_of_countries = CountryList.new.countries_for_continent(params[:continent_name])
     erb :countries, locals: { countries: list_of_countries, continent: params[:continent_name] }
+  end
+
+  def names
+     @database_connection.sql("select name from home")
+
+
+
+    end
+
+
+  def messages
+    @database_connection.sql("select message from home")
+
   end
 
 end
---
+++ db/migrate/1405960319_development_migration.rb
@@ -0,0 +1,9 @@
+class DevelopmentMigration < ActiveRecord::Migration
+  def up
+    # add migration code here
+  end
+
+  def down
+    # add reverse migration code here
+  end
+end
